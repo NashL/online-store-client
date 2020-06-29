ff82529a9869a3a2f84638e687ecac9530f0cad0
Oscar Luza <onluza@gmail.com>
2020-06-29 05:56:20 -0500

    added slideshow component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "../components/NavBar";
+import SlideShow from "../components/SlideShow";
 
 class Home extends Component {
   state = {};
@@ -17,11 +18,12 @@ class Home extends Component {
       <div>
         <NavBar />
         <div className="container main-container">
-        <h1 className="title">Bienvenidos a tu Tienda Online</h1>
-        <button onClick={this.signInHandler}> INGRESAR</button>
-        <button onClick={this.signUpHandler}> REGISTRARSE</button>
+          <div className="slider-content">
+            <SlideShow />
+          </div>
+          <button onClick={this.signInHandler}> INGRESAR</button>
+          <button onClick={this.signUpHandler}> REGISTRARSE</button>
         </div>
-
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import bulmaCarousel from "../node_modules/bulma-carousel/dist/js/bulma-carousel.min.js";
+
 import './styles/index.scss'
 
 ReactDOM.render(
