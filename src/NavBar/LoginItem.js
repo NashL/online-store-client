373e914bad4fed7e8b8db081dd7ca0c10807f1e0
Oscar Luza <onluza@gmail.com>
2020-07-26 20:12:42 -0500

    using currentUser from jwt decoded

--- src/NavBar/LoginItem.js
+++ src/NavBar/LoginItem.js
@@ -4,15 +4,18 @@ import { CURRENT_USER } from "../graphql/queries";
 import { Link } from 'react-router-dom'
 
 const LoginItem = (props) => {  
-  console.log('curren', props.currentUser)
-  const { loading, error, data } = useQuery(CURRENT_USER);
+  console.log('cuRRent', props.currentUser)
+  const currentUser = props.currentUser
+  // const { loading, error, data } = useQuery(CURRENT_USER);
 
-  if (loading) return   <Link className="navbar-item" to="/login">Inicio de Sesion/Registrarse</Link>;
-  if (error) return     <Link className="navbar-item" to="/login">Inicio de Sesion/Registrarse</Link>;
-  if (data === undefined) return <Link className="navbar-item" to="/login">Inicio de Sesion/Registrarse</Link>;
-  return (
-    <Link className="navbar-item" to="/login">{data.currentUser.fullName}</Link>
-  )
+  if (!!currentUser)
+    return  <Link className="navbar-item" to="/login">{currentUser.fullName}</Link>
+  else
+    return  <Link className="navbar-item" to="/login">Inicio de Sesion/Registrarse</Link>;
+  // if (error) return     <Link className="navbar-item" to="/login">Inicio de Sesion/Registrarse</Link>;
+  // if (data === undefined) return <Link className="navbar-item" to="/login">Inicio de Sesion/Registrarse</Link>;
+  // return (
+  // )
 }
 
 export default LoginItem
